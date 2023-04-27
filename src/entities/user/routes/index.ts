34b4cba15f71b176393117a7cb0c66335dee644a
KIRA <bfakhruz@gmail.com>
2023-04-27 21:01:08 +0500

    edited user router

--- src/entities/user/routes/index.ts
+++ src/entities/user/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userController } from "../controllers";
-import { authMiddleware } from "../../auth";
+import { authMiddleware } from "../../auth/middlewares";
+import { userMiddleware } from "../middlewares";
 
 export const userRouter = Router();
 
@@ -9,9 +10,9 @@ userRouter.get("/getOne", [authMiddleware.isValideJwt], userController.getOne);
 // userRouter.patch("/edit", [authMiddleware.isValideJwt], userController.edit);
 
 // ADMIN COMMANDS
-userRouter.get("/getAllAsAdmin", [authMiddleware.isValideJwt, authMiddleware.idAdminRole], userController.getAllAsAdmin);
-userRouter.get("/getOneAsAdmin", [authMiddleware.isValideJwt, authMiddleware.idAdminRole], userController.getOneAsAdmin);
-userRouter.post("/createAsAdmin", [authMiddleware.isValideJwt, authMiddleware.idAdminRole], userController.createAsAdmin);
-userRouter.patch("/editAsAdmin", [authMiddleware.isValideJwt, authMiddleware.idAdminRole], userController.editAsAdmin);
-userRouter.delete("/deleteAsAdmin", [authMiddleware.isValideJwt, authMiddleware.idAdminRole], userController.deleteAsAdmin);
-userRouter.delete("/deleteManyAsAdmin", [authMiddleware.isValideJwt, authMiddleware.idAdminRole], userController.deleteManyAsAdmin);
+userRouter.get("/getAllAsAdmin", [authMiddleware.isValideJwt, userMiddleware.idAdminRole], userController.getAllAsAdmin);
+userRouter.get("/getOneAsAdmin", [authMiddleware.isValideJwt, userMiddleware.idAdminRole], userController.getOneAsAdmin);
+userRouter.post("/createAsAdmin", [authMiddleware.isValideJwt, userMiddleware.idAdminRole], userController.createAsAdmin);
+userRouter.patch("/editAsAdmin", [authMiddleware.isValideJwt, userMiddleware.idAdminRole], userController.editAsAdmin);
+userRouter.delete("/deleteAsAdmin", [authMiddleware.isValideJwt, userMiddleware.idAdminRole], userController.deleteAsAdmin);
+userRouter.delete("/deleteManyAsAdmin", [authMiddleware.isValideJwt, userMiddleware.idAdminRole], userController.deleteManyAsAdmin);
