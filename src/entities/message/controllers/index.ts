68a0ebab6c398ec5bcdf314919a82d05a107f1ed
KIRA <bfakhruz@gmail.com>
2023-04-27 20:59:16 +0500

    edited message controller

--- src/entities/message/controllers/index.ts
+++ src/entities/message/controllers/index.ts
@@ -12,4 +12,75 @@ export const messageController = {
       res.json({error: error});
     }
   },
+
+  async delete(req: Request, res: Response) {
+    try {
+      const body = await messageService.delete(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
+
+  async deleteMany(req: Request, res: Response) {
+    try {
+      const body = await messageService.deleteMany(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
+
+  async checkMany(req: Request, res: Response) {
+    try {
+      const body = await messageService.checkMany(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
+
+  async edit(req: Request, res: Response) {
+    try {
+      const body = await messageService.edit(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
+
+  // ADMIN COMMANDS
+  async editAsAdmin(req: Request, res: Response) {
+    try {
+      const body = await messageService.editAsAdmin(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
+
+  async deleteAsAdmin(req: Request, res: Response) {
+    try {
+      const body = await messageService.deleteAsAdmin(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
+
+  async deleteManyAsAdmin(req: Request, res: Response) {
+    try {
+      const body = await messageService.deleteManyAsAdmin(req.body);
+      res.json({body: body});
+
+    } catch (error) {
+      res.json({error: error});
+    }
+  },
 }
