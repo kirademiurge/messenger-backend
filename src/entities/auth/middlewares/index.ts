df1122cdf8bdba1522f1f22358cb580e422e1860
KIRA <bfakhruz@gmail.com>
2023-04-27 20:58:32 +0500

    edited auth middleware

--- src/entities/auth/middlewares/index.ts
+++ src/entities/auth/middlewares/index.ts
@@ -1,39 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client";
-import { UserRole, config } from "../../../shared";
-
-const prisma = new PrismaClient();
+import { config } from "../../../shared";
 
 export const authMiddleware = {
-	async isUserNotExist (req: Request, res: Response, next: NextFunction) {
-		try {
-			const user = await prisma.user.findUnique({
-				where: { username: req.body.username },
-			});
-
-			if (user) throw new Error("user exists");
-			next();
-
-		} catch (error) {
-			res.json({error: error});
-		}
-	},
-
-	async isUserExist (req: Request, res: Response, next: NextFunction) {
-		try {
-			const user = await prisma.user.findUniqueOrThrow({
-				where: { username: req.body.username },
-			});
-
-			req.body.foundUser = user;
-			next();
-
-		} catch (error) {
-			res.json({error: "user doesn't exist"});
-		}
-	},
-
 	async isPasswordValid (req:Request, res: Response, next: NextFunction) {
 		try {
 			const user = req.body.foundUser;
@@ -54,20 +23,6 @@ export const authMiddleware = {
 
 			req.body.payload = payload;
 			next();
-	
-		} catch (error) {
-			res.json({error: error});
-		}
-	},
-
-	idAdminRole (req: Request, res: Response, next: NextFunction) {
-		try {
-			const payload = req.body.payload;
-			const userRole = payload.role;
-			const neededRole: UserRole = "ADMIN";
-
-			if ( userRole === neededRole ) next();
-			else throw new Error("wrong role")
 	
 		} catch (error) {
 			res.json({error: error});
