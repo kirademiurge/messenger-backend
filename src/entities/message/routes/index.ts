520625c01efa333713e435b6705ffdc2f53c3ab1
KIRA <bfakhruz@gmail.com>
2023-05-03 23:45:37 +0500

    edited message routes

--- src/entities/message/routes/index.ts
+++ src/entities/message/routes/index.ts
@@ -6,13 +6,16 @@ import { messageMiddleware } from "../middlewares";
 export const messageRouter = Router();
 
 // USER COMMANDS
-messageRouter.post("/create", [authMiddleware.isValideJwt, messageMiddleware.isReceiverExist, messageMiddleware.isMessageNotEmpty], messageController.create);
-// messageRouter.delete("/delete", [authMiddleware.isValideJwt, messageMiddleware.isOwnMessage], messageController.delete);
-// messageRouter.delete("/deleteMany", [authMiddleware.isValideJwt], messageController.deleteMany); // NEED EXTRA MIDDLEWARE
-// messageRouter.patch("/checkMany", [authMiddleware.isValideJwt], messageController.checkMany); // NEED EXTRA MIDDLEWARE
-// messageRouter.patch("/edit", [authMiddleware.isValideJwt], messageController.edit); // NEED EXTRA MIDDLEWARE
+messageRouter.post("/create", [authMiddleware.isValideJwt], messageController.create);
+messageRouter.get("/getAll", [authMiddleware.isValideJwt], messageController.getAll);
+messageRouter.get("/getNew", [authMiddleware.isValideJwt], messageController.getNew);
+messageRouter.delete("/delete", [authMiddleware.isValideJwt, messageMiddleware.isOwnMessage], messageController.delete);
+messageRouter.delete("/deleteMany", [authMiddleware.isValideJwt, messageMiddleware.isOwnMessages], messageController.deleteMany);
+messageRouter.patch("/checkMany", [authMiddleware.isValideJwt, messageMiddleware.isOwnMessages], messageController.checkMany);
+messageRouter.patch("/edit", [authMiddleware.isValideJwt, messageMiddleware.isOwnMessage], messageController.edit);
 
 // ADMIN COMMANDS
-messageRouter.delete("/deleteAsAdmin", [authMiddleware.isValideJwt, messageMiddleware.isOwnMessage], messageController.deleteAsAdmin);
+messageRouter.get("/getAllAsAdmin", [authMiddleware.isValideJwt], messageController.getAllAsAdmin);
+messageRouter.delete("/deleteAsAdmin", [authMiddleware.isValideJwt], messageController.deleteAsAdmin);
 messageRouter.delete("/deleteManyAsAdmin", [authMiddleware.isValideJwt], messageController.deleteManyAsAdmin);
 messageRouter.patch("/editAsAdmin", [authMiddleware.isValideJwt], messageController.editAsAdmin);
